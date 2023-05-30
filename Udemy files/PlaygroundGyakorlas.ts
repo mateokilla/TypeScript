09e5a3b9c485dc3d6b27b95fcaa1c8687bae38d4
mateokilla <nitro_5_bernath_mark@outlook.hu>
2023-05-30 20:04:56 +0200

    interface

--- Udemy files/PlaygroundGyakorlas.ts
+++ Udemy files/PlaygroundGyakorlas.ts
@@ -1,1 +1,10 @@
 //1.
+function randomTombGeneralas(darab: number) {
+    let randomSzamokTombje:number[] = [];
+    for (let i = 0; i < darab; i++) {
+        randomSzamokTombje.push(Math.round(Math.random() * 100))
+    }
+    return randomSzamokTombje
+}
+
+console.log(randomTombGeneralas(10))
